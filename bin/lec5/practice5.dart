e354a4eb657a73204d75fbee70c467e616bcb15e
Funnzone360 <hafizusman7043@gmail.com>
2024-05-10 03:50:17 -0700

    lec5 static data types

---
+++ bin/lec5/practice5.dart
@@ -0,0 +1,53 @@
+/*Data types in Dart
+there are two types of data types in dart 
+No 1 Static Data types
+No 2 dynamic Data types 
+
+
+DEFINE STATIC DATA TYPES : 
+The Data types  that is known at compile time is called static data type.
+int,double,String,bool,var are static data types 
+
+
+DEFINE DYNAMIC DATA TYPES :
+So dynamic data types are the types that are determined at run time 
+dynamic,list,map,set are dynamic data types 
+
+
+
+
+NOTE=> final,const,Static are not data types they are resrved keywords in dart means thaat have  special meaning 
+
+
+
+*/
+
+// void main() {
+// // So In This lecture we will study static data type,s only
+// // int ,double , bool ,String , Var,
+
+//   int integervalue = 100;
+//   double doublevalue = 2.34876;
+//   bool booleanvalue = true;
+//   String stringvalue = "USMAN";
+//   print("This Is Printing Integer,s $integervalue");
+//   print("This Is Printing double $doublevalue");
+//   print("this is printing boolean  value $booleanvalue");
+//   print("this is printing String value $stringvalue");
+// }
+
+// void main(List<String> args) {
+//   //var automatically identifies that which type of data store in a variable
+//   var differentValues = 100;
+//   differentValues = 200;
+//   // differentValues = "hello"; // this line will give,s an error beacuse forst we store integer then it will change an integer values not string or other
+//   print("this is an example of Var $differentValues");
+// }
+
+void main(List<String> args) {
+  //var automatically identifies that which type of data store in a variable
+  var storename = "usman";
+  storename = "malik";
+  // differentValues = 200; // this line will give,s an error beacuse forst we store String then it will change for a String values not integer or other
+  print("this is an example of Var $storename");
+}
