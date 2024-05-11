59814c8b043340e196851fab17f1c4ec1dd19197
Funnzone360 <hafizusman7043@gmail.com>
2024-05-11 09:01:51 -0700

    integer's functions

---
+++ bin/lec6/practice6.dart
@@ -0,0 +1,71 @@
+// lec six static data types int ,double,String,bool , var functions
+
+//FUNCTIONS OF INTEGERS IN DART static data type
+
+void main(List<String> args) {
+  int number1 = 200;
+  int number2 = 9;
+
+  print(number1.isEven); //check number is even  2,4,6,8,10...
+  print(number2.isEven); //check number is even  2,4,6,8,10...
+
+  print(number1.isOdd); //check number is odd  3,5,7,9,11...
+  print(number2.isOdd); //check number is odd  3,5,7,9,11...
+
+  print(number1.isFinite); // countable
+  print(number2.isFinite); // countable
+
+  print(number1.isInfinite); //uncountable
+  print(number1.isInfinite); //uncountable
+
+  print(number1.isNaN); //is not a number
+  print(number2.isNaN); //is not a number
+
+  print(number1.isNegative); //check number is negative
+  print(number2.isNegative); //check number is negative
+
+  print(number1.roundToDouble()); //convert integer value to double
+  print(number2.roundToDouble()); //convert integer value to double
+
+  print(number1
+      .round()); // Rounds the integer to the nearest whole number  3.6 to 4
+  print(number2
+      .round()); //Rounds the integer to the nearest whole number   -2.8  to -3
+
+  print(number1.bitLength); //bit representation
+  print(number2.bitLength); //bit representation
+
+  print(number1.hashCode); //hash code
+  print(number1.hashCode); // hash code
+
+  print(number1.sign); //return 1 for positive
+  print(number2.sign); //return -1 for negative
+
+  print(number1.toSigned(2));
+  print(number2.toSigned(2));
+
+  print(number1.truncateToDouble()); //convert integer to double
+  print(number2.truncateToDouble()); //convert integer to double
+
+  print(number1.runtimeType); //tell me the type of variable int
+  print(number2.runtimeType); //tell me the type of variable int
+
+  print(number1.abs()); //abs(absolute value)  convert minus value to positive
+  print(number2.abs()); //abs(absolute value)  convert minus value to positive
+
+  print(number1
+      .ceil()); //return smallet number greter then original number  6 output 5.2
+  print(number2
+      .ceil()); //return smallet number greter then original number  -3 output -3.8
+
+  print(number1.ceilToDouble());
+  print(number2.ceilToDouble());
+
+  print(number1.compareTo(number2));
+
+  print(number1.floor()); //
+
+  print(number1.modInverse(number2));
+
+  print(number1.gcd(number2));
+}
