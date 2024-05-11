804bb33913817ccbcfba9a8f589d02862a1c7fd1
Funnzone360 <hafizusman7043@gmail.com>
2024-05-11 09:15:29 -0700

    lecture 7 double funcions

---
+++ bin/lec7/practice7.dart
@@ -0,0 +1,27 @@
+void main(List<String> args) {
+  double number1 = 2.145;
+  double number2 = 34.45;
+
+  print(number1.sign); //return -1 and +1
+  print(number2.sign); //return -1 and +1
+
+  print(number1.abs());
+  print(number2.abs());
+
+  print(number1.ceilToDouble());
+  print(number2.ceilToDouble());
+
+  print(number1.floorToDouble());
+  print(number2.floorToDouble());
+
+  print(number1.remainder(number2));
+  print(number1.roundToDouble());
+
+  print(number1.toDouble());
+  print(number2.toDouble());
+
+  print(number1.truncateToDouble());
+  print(number2.truncateToDouble());
+
+  print(number1.round());
+}
