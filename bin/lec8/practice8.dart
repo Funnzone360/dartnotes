e134197556d14fa0dcaf3d4c3cf99782a2f9d3f3
Funnzone360 <hafizusman7043@gmail.com>
2024-05-12 01:43:15 -0700

    lecture 8 String functions

---
+++ bin/lec8/practice8.dart
@@ -0,0 +1,41 @@
+void main(List<String> args) {
+  String name1 = "Usman malik";
+  String name2 = "jawad";
+  String result = name1 + name2;
+
+  print(name1.trim());
+  print(name2.trim());
+  print(result);
+
+  print(name1.trimLeft());
+  print(name2.trimLeft());
+
+  print(name1.trimRight());
+  print(name2.trimRight());
+
+  print(name1.toUpperCase());
+  print(name2.toUpperCase());
+
+  print(name1.toString());
+  print(name1.toString());
+
+  print(name1.toLowerCase());
+  print(name2.toLowerCase());
+
+  print(name1.substring(2, 2));
+
+  print(name2.startsWith("q"));
+  print(name2.splitMapJoin(name1));
+
+  print(name1.split(name2));
+
+  print(name2.replaceRange(1, 4, "usama"));
+
+  print(name2.replaceFirstMapped("jawad", (match) => "boy"));
+
+  print(name1.replaceFirst("jawad", "haleem"));
+
+  print(name2.padRight(3));
+
+  print(name1.padLeft(2));
+}
