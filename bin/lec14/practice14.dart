d88f68dd843711fa1e0a205aa4048cf81fc26426
Funnzone360 <hafizusman7043@gmail.com>
2024-05-15 05:24:00 -0700

    setin dart

---
+++ bin/lec14/practice14.dart
@@ -0,0 +1,40 @@
+//set is the unorder collections of different values of same type.
+//set don,t contains douplicate values
+//represetned by {} curly braces
+
+/*
+void main() {
+  Set setofmixvalues = {12, 34, 54, "usman", "ali", true, 34.564};
+  Set<String> setoffixvalues = { "only ", "takes","string" };
+  print(setofmixvalues);
+  print(setoffixvalues);
+
+}
+*/
+
+/*
+void main(List<String> args) {
+  Set micvalues = {"usman", "Ali", 2, 3, 4.554};
+  print(micvalues.isEmpty); //check set is empty
+  print(micvalues.isNotEmpty); //check set is not  empty
+  print(micvalues.first); //get first element in set
+  print(micvalues.last); //get last element in set
+  print(micvalues.length); //check length
+  print(micvalues.hashCode); //check hash code
+  // print(micvalues.single);//set contains only one element
+}
+
+*/
+
+void main(List<String> args) {
+  Set mixvalues = {"malik", "usman", "ali", 2.345, 1, 2, 3, true};
+  Set mixvalues2 = {"malik", "usman", "ali", 3, true};
+  mixvalues.add("younis");
+  mixvalues.addAll([22.33, 456]);
+  mixvalues.remove("malik");
+  mixvalues.contains("ali");
+  // mixvalues.clear();
+  mixvalues.union(mixvalues2);
+  mixvalues.containsAll(mixvalues2);
+  print(mixvalues);
+}
