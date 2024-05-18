3ac92d902d492e9f0617c4707f97db96cb3090fd
Funnzone360 <hafizusman7043@gmail.com>
2024-05-18 01:59:54 -0700

    all loops

---
+++ bin/lec21/practice21.dart
@@ -0,0 +1,74 @@
+// The forEach method is used to iterate over
+// the elements of an iterable (such as lists) and perform an action for each element.
+
+/*
+void main(List<String> args) {
+  List<int> numbers = [1, 2, 3, 4, 5];
+  numbers.forEach((element) {
+    print("$element");
+  });
+}
+
+
+*/
+
+
+
+
+
+/*
+// The for loop is used to iterate a specific number of times.
+void main() {
+  for (int i = 0; i < 5; i++) {
+    print(i);
+  }
+}
+
+*/
+
+
+
+
+
+
+/*
+// The do-while loop is used to execute a block of code repeatedly
+// until a specified condition becomes false. The code block is executed at least once, even if the condition is initially false.
+void main() {
+  int i = 0;
+  do {
+    print(i);
+    i++;
+  } while (i < 5);
+}
+
+
+*/
+
+
+
+
+
+/*
+// The while loop is similar to the do-while loop,
+// but the condition is checked before executing the code block.
+
+void main() {
+  int i = 0;
+  while (i < 5) {
+    print(i);
+    i++;
+  }
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
