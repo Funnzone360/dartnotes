88a3161f0c0f8b9926a246ef190073c7caf7b56f
Funnzone360 <hafizusman7043@gmail.com>
2024-05-18 03:10:25 -0700

    lec22 functions

---
+++ bin/lec22/practice22.dart
@@ -0,0 +1,31 @@
+// FUNCTION IN DART
+// a function is a block of code that is used
+// to perform a specific task or call this where it is needed
+
+/*
+void greet(String name, [String? message]) { //parameter ,Arguments 
+  if (message != null) {
+    print("hello $name $message");
+  } else {
+    print("Hello $name");
+  }
+}
+
+void main(List<String> args) {
+  greet("Usman");
+  greet("Usman", "Gamer");
+}
+*/
+
+
+/*
+String greet(String name, String timeOfDay) {//parameter ,Arguments 
+  return 'Good $timeOfDay, $name!';
+}
+
+void main() {
+  print(greet('Alice', 'morning')); // Output: Good morning, Alice!
+  print(greet('Bob', 'afternoon')); // Output: Good afternoon, Bob!
+  print(greet('Charlie', 'evening')); // Output: Good evening, Charlie!
+}
+*/
