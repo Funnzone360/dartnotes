c67b3ecf6d9e9fbf3ab36df9911875583e0983a2
Funnzone360 <hafizusman7043@gmail.com>
2024-05-18 05:14:50 -0700

    constructor

---
+++ bin/ooplec24/practice24.dart
@@ -0,0 +1,25 @@
+// A constructor is a different type of function which is created with same name as its class name
+//constructor automatically called when object of that class is created
+//no return type of constructor
+
+
+
+/*
+class Person {
+  String name;
+  int age;
+
+  // Simplified Constructor
+  Person(this.name, this.age);
+
+  void introduce() {
+    print('Hi, my name is $name and I am $age years old.');
+  }
+}
+
+void main() {
+  var person = Person('Alice', 30);
+  person.introduce(); // Output: Hi, my name is Alice and I am 30 years old.
+}
+
+*/
