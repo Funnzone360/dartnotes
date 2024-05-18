418b9b438188319014d8fe42f823e6c2c19d9dbe
Funnzone360 <hafizusman7043@gmail.com>
2024-05-18 01:11:30 -0700

    break(stop) and contine(skip)

---
+++ bin/lec19/practice19.dart
@@ -0,0 +1,49 @@
+// Desion making statement      if else  and switch  etc
+//jump statement                break and continue
+//looping statement             foreach  for  do while  while
+
+/*
+void main(List<String> args) {
+  String name = "Usman";
+
+  if (name.contains("Usman")) {
+    print("Name contained $name");
+  }
+
+  if (name != "Usman") {
+    print("Name Not contained $name");
+  }
+}
+*/
+
+/*
+void main(List<String> args) {
+  String name = "Usman";
+  if (name is String) {
+    print("Name is String $name");
+  } else if (name is! String) {
+    print(" name is not a String $name");
+  } else {
+    print("other data type ");
+  }
+}
+*/
+
+
+/*
+void main(List<String> args) {
+  int number = 3;
+  switch (number) {
+    case 1:
+      print("Number is one 1");
+      break;
+    case 2:
+      print("Number is two 2");
+      break;
+    case 3:
+      print("Number is three 3");
+      break;
+    default:
+  }
+}
+*/
---
+++ bin/lec20/practice20.dart
@@ -0,0 +1,56 @@
+//break   and continue
+
+/*
+//break: It is used to exit
+// the nearest loop (enclosing the break statement),
+// prematurely terminating the loop's execution.
+void main() {
+  for (int i = 1; i <= 5; i++) {
+    if (i == 3) {
+      break; // Exit the loop when i equals 3
+    }
+    print('Counting: $i');
+  }
+  print('Loop ended.');
+}
+*/
+
+
+
+
+
+
+
+/*
+// continue: It is used to skip the
+// remaining code inside the loop for the current iteration and proceed to the next iteration of the loop.
+
+void main() {
+  for (int i = 1; i <= 5; i++) {
+    if (i == 3) {
+      continue; // Skip the current iteration when i equals 3
+    }
+    print('Counting: $i');
+  }
+  print('Loop ended.');
+}
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
