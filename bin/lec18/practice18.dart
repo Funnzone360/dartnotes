602e2dca344f9101ea4fefae0e297c2e2bf3bea0
Funnzone360 <hafizusman7043@gmail.com>
2024-05-18 00:20:00 -0700

    enum in dart

---
+++ bin/lec18/practice18.dart
@@ -0,0 +1,39 @@
+//enamuration is used to give names to constant  for example 2 we can give nmae two
+
+enum Day {
+  monday,
+  tuesday,
+  wednesday,
+  thursday,
+  friday,
+  saturday,
+  sunday,
+}
+
+void main() {
+  var today = Day.tuesday;
+
+  switch (today) {
+    case Day.monday:
+      print("It's Monday, the start of the week.");
+      break;
+    case Day.tuesday:
+      print("It's Tuesday, still early in the week.");
+      break;
+    case Day.wednesday:
+      print("It's Wednesday, halfway there!");
+      break;
+    case Day.thursday:
+      print("It's Thursday, almost to the weekend.");
+      break;
+    case Day.friday:
+      print("It's Friday, time to celebrate!");
+      break;
+    case Day.saturday:
+      print("It's Saturday, enjoy the weekend!");
+      break;
+    case Day.sunday:
+      print("It's Sunday, time to relax.");
+      break;
+  }
+}
