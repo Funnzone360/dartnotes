18a0d7b07262b7e4042cf7df44a04c86076cb613
Funnzone360 <hafizusman7043@gmail.com>
2024-05-18 05:00:54 -0700

    object and classis

---
+++ bin/ooplec23/practice23.dart
@@ -0,0 +1,45 @@
+// Classes are blueprints for creating objects (instances).
+// Each object has its owen properties(attributes) and function
+//properties may be different like color of person some has white some black their height tall ,and small etc
+//but there function may be same like a person can eat , sleep , walk, run ,etc
+
+
+/*
+class Person {
+  // Properties (Attributes)
+  String name;
+  String color;
+  double height;
+
+  Person(this.name, this.color, this.height);
+
+  void eat() {
+    print('$name is eating.');
+  }
+
+  void sleep() {
+    print('$name is sleeping.');
+  }
+
+  void walk() {
+    print('$name is walking.');
+  }
+
+  void run() {
+    print('$name is running.');
+  }
+}
+
+void main() {
+  var person1 = Person('Alice', 'White', 1.65);//object of person class
+  var person2 = Person('Bob', 'Black', 1.80);//object of person class
+
+  print('Person 1: ${person1.name}, ${person1.color}, ${person1.height}m');
+  person1.eat();
+  person1.sleep();
+
+  print('Person 2: ${person2.name}, ${person2.color}, ${person2.height}m');
+  person2.walk();
+  person2.run();
+}
+*/
