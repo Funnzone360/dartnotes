2ef14815374ee90b351ff6c863f1b17ccca64f1d
Funnzone360 <hafizusman7043@gmail.com>
2024-05-18 03:48:43 -0700

    dart functions recursion function

--- bin/lec22/practice22.dart
+++ bin/lec22/practice22.dart
@@ -1,4 +1,4 @@
-// FUNCTION IN DART
+// FUNCTION IN DART Recursion in Dart
 // a function is a block of code that is used
 // to perform a specific task or call this where it is needed
 
@@ -17,7 +17,6 @@ void main(List<String> args) {
 }
 */
 
-
 /*
 String greet(String name, String timeOfDay) {//parameter ,Arguments 
   return 'Good $timeOfDay, $name!';
@@ -27,5 +26,24 @@ void main() {
   print(greet('Alice', 'morning')); // Output: Good morning, Alice!
   print(greet('Bob', 'afternoon')); // Output: Good afternoon, Bob!
   print(greet('Charlie', 'evening')); // Output: Good evening, Charlie!
+}
+*/
+
+// Recursion in dart
+
+// A function which is called itself again and again or recursively, then this process is called recursion.
+// It is used to solve the complex problem by dividing it into sub-part.
+
+/*
+int factorial(int n) {
+  if (n <= 1) {
+    return 1; // Base case
+  } else {
+    return n * factorial(n - 1); // Recursive case
+  }
+}
+
+void main() {
+  print(factorial(5)); // Output: 120
 }
 */
